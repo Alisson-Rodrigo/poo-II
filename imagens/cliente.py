75c4f69243e56948401fadb73a6a4e473a2b81a3
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-07-05 09:41:40 -0300

    ok

---
+++ imagens/cliente.py
@@ -0,0 +1,30 @@
+import socket
+
+# Configurações do cliente
+host = 'localhost'
+port = 7000
+
+# Tamanho do buffer para leitura e recebimento dos dados
+buffer_size = 4096
+
+# Cria o socket do cliente
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((host, port))
+print('Conexão estabelecida com o servidor.')
+
+# Abre o arquivo para salvar o vídeo recebido
+video_file = open('video_received.mp4', 'wb')
+
+# Recebe os pacotes de dados do servidor e escreve no arquivo
+while True:
+    # Recebe os dados do servidor
+    data = client_socket.recv(buffer_size)
+    if not data:
+        # Fim da transmissão
+        break
+    # Escreve os dados no arquivo
+    video_file.write(data)
+
+# Fecha o arquivo e o socket
+video_file.close()
+client_socket.close()
--- sistema/operacoes.py
+++ sistema/operacoes.py
@@ -5,7 +5,7 @@ import threading, socket
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="1234",
+    password="Curupira098*",
     #linux: Curupira098*
     database="bdPOO" 
 )
