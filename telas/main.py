2f12de0edf96540b48b3114b5dfa7f55950f8b96
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-07-10 22:49:43 -0300

    ok

--- telas/main.py
+++ telas/main.py
@@ -78,7 +78,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
         self.setupUi(self)
         hostname = socket.gethostname()
         ip_Adress = socket.gethostbyname(hostname)
-        ip = ip_Adress
+        ip = '10.0.0.176'
         port = 10005
         addr = ((ip, port))
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -273,48 +273,39 @@ class VideoPlayer(QWidget):
 
         self.video_path = video_path
         
-        # Definindo o background na cor preta
         palette = self.palette()
         palette.setColor(QPalette.Background, QColor(0, 0, 0))
         self.setPalette(palette)
 
-        # Criando o layout principal
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        # Criando o widget de vídeo
         self.video_widget = QVideoWidget()
         self.layout.addWidget(self.video_widget)
 
-        # Criando o layout para as opções
         self.options_layout = QHBoxLayout()
         self.layout.addLayout(self.options_layout)
 
-        # Criando o botão para reproduzir/pausar o vídeo
         self.play_pause_button = QPushButton()
         self.play_pause_button.setFixedSize(32, 32)
         self.play_pause_button.setStyleSheet("background-color: black")
         self.play_pause_button.clicked.connect(self.toggle_play_pause)
         self.options_layout.addWidget(self.play_pause_button)
 
-        # Criando a barra de reprodução
         self.slider = QSlider(Qt.Horizontal)
         self.slider.setRange(0, 0)
         self.slider.sliderMoved.connect(self.set_position)
         self.options_layout.addWidget(self.slider)
 
-        # Carregando as imagens dos botões de reprodução e pausa
         self.play_image = QIcon(QPixmap("play.svg"))
         self.pause_image = QIcon(QPixmap("pause.svg"))
 
-        # Criando o player de mídia
         self.media_player = QMediaPlayer()
         self.media_player.setVideoOutput(self.video_widget)
         self.media_player.stateChanged.connect(self.video_state_changed)
         self.media_player.positionChanged.connect(self.update_position)
         self.media_player.durationChanged.connect(self.update_duration)
 
-        # Flag para controlar a reprodução do vídeo
         self.is_playing = False
 
         self.set_video_path(self.video_path)
