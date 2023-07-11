c2c618a8e5bd547752605de43add942dadba0439
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-07-11 08:59:11 -0300

    sim

--- telas/main.py
+++ telas/main.py
@@ -79,7 +79,7 @@ class Main(QtWidgets.QMainWindow, Ui_Main):
         hostname = socket.gethostname()
         ip_Adress = socket.gethostbyname(hostname)
         ip = '10.180.44.22'
-        port = 10006
+        port = 10010
         addr = ((ip, port))
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client_socket.connect(addr)
